1188ad7c1868744fe2d3f9255d85a88e24ec8114
Prince Panwar <princepanwar420420@gmail.com>
2025-12-26 16:16:58 +0000

    Fixed blur issue if profile pic

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,32 +1,38 @@
 import React from "react";
-import AboutPic from "../images/profile-img/about.png";
+import AboutPic from "../images/profile-img/coding.png";
 
 const About = () => {
   return (
     <section className="about section" id="about">
       <h2 className="section-title">About</h2>
 
-      <div className="about__container bd-grid">
-        <div className="about__img">
+      <div class="about__container bd-grid">
+        <div class="about__img">
           <img src={AboutPic} alt="" />
         </div>
 
         <div>
-          <h2 className="about__subtitle">I'm Prince Panwar</h2>
-          <p className="about__text">
-            Hi there 👋, I'm Prince Panwar, a highly skilled full stack
-            developer with a wide range of expertise in web development and
-            programming. I have a strong foundation in essential web
-            technologies such as HTML, CSS, and JavaScript. I have built a
-            reputation for delivering innovative and functional web solutions. I
-            have created various projects.
+          <p class="about__text">
+            Hi👋, My name is <b>Prince Panwar</b>. I'm a{" "}
+            <b>front-end web developer</b> with 1 year of experience in building
+            web applications. I work mainly with <b>React.js</b> and{" "}
+            <b>TypeScript</b> to create responsive and user-friendly
+            applications.
           </p>
           <p>
-            I have also won two trophies 🏆 in debugging competitions, securing
-            3rd and 2nd positions respectively. These achievements have
-            instilled in me a sense of discipline and perseverance, which I
-            bring to every project I work on. I am excited to bring my skills
-            and experience to a new role and contribute to a team of developers.
+            As my core expertise lies in React, where I focus on writing
+            scalable, maintainable, and type-safe code.
+          </p>
+          <br />
+          <p>
+            Alongside React, I use <b>Jest</b> for unit testing, helping catch
+            bugs early and ensuring components work as intended.
+          </p>
+          <br />
+          <p>
+            Being part of a small team has given me the chance to handle
+            different responsibilities, improve problem-solving, and deliver
+            applications that work in real use.
           </p>
         </div>
       </div>
